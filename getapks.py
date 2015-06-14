d59a80e2dce149c30fb9122f96cc8b4cffd3334a
flycode310 <cyy_ahut@yeah.net>
2015-06-14 23:23:24 +0800

    use getapks.py to get apk from server

--- getapks.py
+++ getapks.py
@@ -12,22 +12,20 @@ try:
     conn = MySQLdb.connect(host='localhost', user='root', passwd='root', db='asec', port=3306, charset='utf8')
     print '连接成功'
     cur = conn.cursor()
-    sqlString = "select c.app_url_bin " \
+    sqlString = "select c.app_url_bin, b.category_name " \
                 "from asec.asec_app as a, asec.asec_app_category as b, asec.asec_app_meta as c " \
                 "where a.app_id = c.app_id and a.app_category_ids = b.category_id " \
-                "and b.category_name = \'社交通信\' and c.app_url_bin is not null " \
-                "order by a.app_id;"
-    sqlString1 = "select * from asec.asec_app_category"
+                "and c.app_url_bin is not null;"
     cur.execute(sqlString)
     regexString = '/M00'
     count = 0
     for data in cur.fetchall():
-        srcpath = u'/var/fastdfs/' + re.sub(regexString,'/data',data[0])
-        despath = u'/opt/apks/社交通信'
-        os.system(('cp %s %s' % (srcpath, despath)).encode('gbk'))
-        # shutil.copy(srcpath, despath)
+        srcpath = '/var/fastdfs/' + re.sub(regexString, '/data', data[0])
+        despath = 'chen.yayun:~/local/apks/tmp/' + data[1] + '/' + data[0].split('/')[4] + '.apk'
+        os.system(('scp ' + srcpath + ' ' + despath).encode("utf-8"))
         count += 1
-        print '成功复制' + str(count) + '条数据'
+        print '成功复制第' + str(count) + '条数据到' + despath.encode("utf-8")
+
     conn.commit()
     cur.close()
     conn.close()
