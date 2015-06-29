a24ee4fe1893d7231cf7adccf24a68e2d0e64bde
chenyayun <cyy_ahut@yeah.net>
2015-06-29 17:57:22 +0800

    modified getss.py to add gen_power

--- getss.py
+++ getss.py
@@ -41,9 +41,50 @@ def gen_ss():
     # close file
     for index in range(0, 11):
         files_list[index].close()
-    conn.commit()
     cur.close()
+    conn.commit()
     conn.close()
 
+
+def gen_power():
+    # query the mysql
+    conn = MySQLdb.Connect(host="localhost", user="root", passwd="root", db="asec", port=3306, charset='utf8')
+    cur = conn.cursor()
+    sql_string = "select * from asec.asec_app_statistics"
+    cur.execute(sql_string)
+    # get power_dict for every call without parameters
+    power_dict = {}
+    for item in cur.fetchall():
+        call_no_para = '<' + item[0].split('(')[0] + '()>'
+        power_list = []
+        for i in range(1, 12):
+            power_list.append(item[i])
+        if call_no_para not in power_dict:
+            power_dict[call_no_para] = power_list
+        else:
+            tmp_list = power_dict[call_no_para]
+            for i in range(0, 11):
+                tmp_list[i] += power_list[i]
+    cur.close()
+    conn.commit()
+    conn.close()
+
+    files_dir = "powerconfig/"
+    files_list = []
+    # open file
+    for index in range(0, 11):
+        files_list.append(open(files_dir + str(index + 1) + '.txt', 'w+'))
+    # write file
+    for call in power_dict:
+        power_list = power_dict[call]
+        for index in range(0, 11):
+            if power_list[index] != 0.0:
+                files_list[index].write(call + ';' + str(power_list[index]) + '\n')
+    # close file
+    for index in range(0, 11):
+        files_list[index].close()
+
+
 if __name__ == '__main__':
-    gen_ss()
+    # gen_ss()
+    gen_power()
